52382fc6f14b0de8e2dc3131176a6706e3b7d5d5
MattheusOliveira <mattheus.exe@hotmail.com>
2018-05-09 21:41:25 -0300

    getters e setters do cliente

--- src/main/java/Model/Cliente.java
+++ src/main/java/Model/Cliente.java
@@ -1,5 +1,16 @@
 package Model;
 
 public class Cliente {
+     
+	private String nome;
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	 
+	
 }
